e01674f91d2501df6ae663859cad4bd015fd7ee9
DAyyappan <dineshayyappan90@gmail.com>
2017-10-20 17:04:21 -0400

    added function to evaluate seating chart

--- generateCharts.py
+++ generateCharts.py
@@ -6,10 +6,12 @@ import csv
 import time
 
 
+
+def importClass(fileName):
 # Import class from .csv in project folder
 # arg filename -- name of file
 # return studentInfo -- list of dictionaries
-def importClass(fileName):
+
     #use csv to open file and skip header row
     f = open(fileName)
     csv_f = csv.reader(f)
@@ -41,17 +43,27 @@ def importClass(fileName):
 
     return studentInfo
 
+
+
+
 def generateRandomSeatingChart(studentInfo, numGroups, groupSize):
-    #initialize empty seating chart
+# randomly generate seating chart for all students given number of groups and group size
+# arg studentInfo -- list of dictionaries for each student
+# arg numGroups -- int number of groups
+# arg groupSize -- int size of groups
+# return seatingChart -- numGroups x groupSize matrix of seating assignments
+
+    # initialize empty seating chart
     seatingChart = np.zeros((numGroups,groupSize))
 
-    #track which students have been assigned
+    # track which students have been assigned
     assigned = np.zeros(len(studentInfo), dtype = bool)
 
-    #initialize seat/group counters
+    # initialize seat/group counters
     groupNum = 0
     seatNum = 0
 
+    # assign random seat to each student filling in seat 1 for every group, then seat 2, etc.
     for student in range(1, len(studentInfo)):
         while True:
             n = np.random.random_integers(1, len(studentInfo)-1)
@@ -75,15 +87,56 @@ def generateRandomSeatingChart(studentInfo, numGroups, groupSize):
 
     return seatingChart
 
+
+def evaluateSeatingChart(seatingChart, studentInfo, FMR):
+# score seating chart based on student preferences
+# arg seatingChart -- matrix of students in groups
+# arg studentInfo -- list of dictionaries of student Info
+# arg FMR -- dictionary of which tables in classroom are front, middle, reader
+# return score -- int score of seatingChart (-1 is invalid; greater is better)
+
+    score = 0
+
+    for group in range(len(seatingChart)):
+        negPairs = []
+        for seat in range(len(seatingChart[0])):
+            student = int(seatingChart[group][seat])
+            negPairs.append(studentInfo[student]['NegativePairs'])
+
+            # if any negative pair is in this group, return -1
+            if student in negPairs:
+                #debugging
+                #print("Conflict check failed because Student %d shouldn't sit with " % student + str(studentInfo[student]['NegativePairs']) + " but their group is " + str(seatingChart[group]))
+                return -1
+
+            # if any positive pair is in this group, +1
+            for pos in studentInfo[student]['PositivePairs']:
+                if pos in seatingChart[group]:
+                    score += 1
+
+            # if student is sitting where they prefer, +1
+            # if student is sitting the opposite of where they prefer, -2
+            for loc in studentInfo[student]['LocationPref']:
+                if (group+1) in FMR[loc]:
+                    score += 1
+                if ((loc == 'F') and ((group+1) in FMR['R'])) or ((loc == 'R') and ((group+1) in FMR['F'])):
+                    score -= 2
+
+    return score
+
 def randomGen():
+# execute and time random generation THEN conflict checking method of seating chart generation
     tStart = time.clock()
     studentInfo = importClass('StudentData.csv')
     numGroups = 6
     groupSize = 5
+    classArrangement = {'F':[1,2], 'M':[3,4], 'R':[5,6]}
 
     seatingChart = generateRandomSeatingChart(studentInfo, numGroups, groupSize)
+    score = evaluateSeatingChart(seatingChart, studentInfo, classArrangement)
 
     print(str(seatingChart))
+    print ("Seating chart score: %d" % score)
     tEnd = time.clock()
     print(str(tEnd-tStart))
 
@@ -96,5 +149,5 @@ randomGen()
 # Create and comment the following functions:
 #   generateValidSeatingChart
 #   generateRandomSeatingChart -- DONE
-#   evaluateSeatingChart
+#   evaluateSeatingChart -- DONE
 #   exportClassesToCSV
