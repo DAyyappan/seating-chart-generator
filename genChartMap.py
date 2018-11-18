38d1feb35df55914c4d446b7474ad3b8b08e1cad
DAyyappan <dineshayyappan90@gmail.com>
2018-11-18 16:19:24 +0530

    outlining input/transform/output of key functions

--- genChartMap.py
+++ genChartMap.py
@@ -7,17 +7,52 @@ import math
 
 
 # function read student information from csv
+#   input: fileName
+#   transform: read contents of fileName
+#   output: array of student information studentInfo
+
 
 # function input user constraints (groups, groupsize, number of charts)
+#   input: number of groups, group size, number of charts, F/M/R orientation
+#   transform: store inputs as system variables
+#   output: array user constraints
 
 # function generate chart
+#   input: number of groups, group size, studentInfo
+#   transform: allocate all students into evenly populated groups
+#   output: seating chart matrix
 
 # function score chart
+#   input: seating matrix, student info
+#   transform: compare matrix to student info
+#   output: score for specific chart
 
 # function find distance between two charts
+#   input: matrix 1, matrix 2
+#   transform: compare distance between groups (frob. norm? euclidean distance?)
+#   output: distance between 1 and 2
 
 # function analyze and display results
+#   input: top matrices, scores, and distances between them
+#   transform: choose the best (numCharts) seats (combo of score and distances)
+#   output:
+
+# function convert matrix to seating chart
+#   input:
+#   transform:
+#   output:
 
 # function export results to CSV
+#   input:
+#   transform:
+#   output:
 
 # execute
+    # read student info
+    # input user constraints
+    #     Loop:
+    #         generate random chart
+    #         score chart
+    # find distance between top charts
+    # analyze and display results
+    # export charts and analysis to csv
