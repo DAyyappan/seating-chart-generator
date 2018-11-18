211cdbf8cc666c6652d4487541f4034ef3dd106b
DAyyappan <dineshayyappan90@gmail.com>
2018-11-18 15:54:13 +0530

    program outline

---
+++ genChartMap.py
@@ -0,0 +1,23 @@
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import time
+import math
+
+
+# function read student information from csv
+
+# function input user constraints (groups, groupsize, number of charts)
+
+# function generate chart
+
+# function score chart
+
+# function find distance between two charts
+
+# function analyze and display results
+
+# function export results to CSV
+
+# execute
